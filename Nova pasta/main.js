bca11903811448271d1ebe7a65418e95d0913ee8
João Guilherme <joah.guilherme@gmail.com>
2022-06-05 17:51:01 -0300

    Add files via upload

---
+++ Nova pasta/main.js
@@ -0,0 +1,87 @@
+function start(){
+    location.href= "level1.html";
+}
+function back(){
+    location.href= "home.html";
+}
+
+function answer1(){
+    var aswr1 = document.getElementById("answeri").value
+    if( aswr1 == "Rainbow"){
+        location.href= "level2.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer2(){
+    var aswr2 = document.getElementById("answerii").value
+    if( aswr2 == "Blackout"){
+        location.href= "level3.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer3(){
+    var aswr3 = document.getElementById("answeriii").value
+    if( aswr3 == "@_.ast3roid"){
+        location.href= "level4.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer4(){
+    var aswr4 = document.getElementById("answeriv").value
+    if( aswr4 == "I am here"){
+        location.href= "level5.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer5(){
+    var aswr5 = document.getElementById("answerv").value
+    if( aswr5 == "I know what you did last summer"){
+        location.href= "level6.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer6(){
+    var aswr6 = document.getElementById("answervi").value
+    if( aswr6 == "Security"){
+        location.href= "level7.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer7(){
+    var aswr7 = document.getElementById("answervii").value
+    if( aswr7 == "Fort Lauderdale"){
+        location.href= "level8.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer8(){
+    var aswr8 = document.getElementById("answerviii").value
+    if( aswr8 == "Galileu Galilei"){
+        location.href= "level9.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer9(){
+    var aswr9 = document.getElementById("answerix").value
+    if( aswr9 == "Easy level"){
+        location.href= "level10.html"
+    }else{
+        alert('Try again')
+    }
+}
+function answer10(){
+    var aswr10 = document.getElementById("answerx").value
+    if( aswr10 == "RJJK"){
+        location.href= "end.html"
+    }else{
+        alert('Try again')
+    }
+}
